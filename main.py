ce7e52c44d40139255c2cd30e72e6172f997f8ce
dasmunoz123 <daasanchezmu@unal.edu.co>
2023-11-10 20:00:48 -0500

    Flask API with logistic regression: Tested from postman in localhost enviroment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import traceback
-from flask import Flask, jsonify
-from flask import request
+from flask import Flask, jsonify, request
 from flask import make_response
 from flask import redirect
 from flask import render_template
@@ -12,8 +11,8 @@ model_file_name = 'MLmodels/lr_model.pkl'
 model_columns_file_name = 'MLmodels/lr_model_columns.pkl'
 app = Flask(import_name=__name__)
 
-@app.route(rule='/landing', methods=['POST','GET'])
-def test():
+@app.route(rule='/')
+def home():
     return "Welcome to machine learning model APIs!"
 
 
@@ -21,13 +20,13 @@ def test():
 def predict():
     if ml_model:
         try:
-            request_json = request.json
-            request_df = pd.DataFrame(data=request_json)
+            req = request.json
+            request_df = pd.DataFrame(data=req,index=[0])
             query = pd.get_dummies(data=request_df)
-            query = query.reindex(columns=ml_columns, fill_value=0)
-            prediction = list(ml_model.predict(query))
-            
-            return jsonify({'Predictions': list(prediction)})
+            query = query.reindex(columns=ml_columns, fill_value=False)
+            predictions = ml_model.predict(query)
+                        
+            return jsonify({'Predictions': predictions.tolist()})
         
         except:
             return jsonify({'trace': traceback.format_exc()})
@@ -43,5 +42,5 @@ if __name__ == '__main__':
     except: port_number = 5001
     ml_model = joblib.load(model_file_name)
     ml_columns = joblib.load(model_columns_file_name)
-    print(f"Machine Learning model succesfully loaded")
+    print(f"\n Machine Learning Model Succesfully Loaded \n")
     app.run(port=port_number, debug=True)
